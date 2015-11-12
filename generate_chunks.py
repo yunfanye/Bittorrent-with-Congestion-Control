d1abe3de7fe92642d33ebdf61575b4c1ef00fca7
Jin Hu <thuhujin@gmail.com>
2015-11-12 00:52:06 -0500

    bug chunk id fix

---
+++ generate_chunks.py
@@ -0,0 +1,17 @@
+output_1 = "1.haschunks"
+output_2 = "2.haschunks"
+output_3 = "master.haschunks"
+out_file = open(output_1, "w")
+chunks = 2000
+for i in range(chunks):
+	out_file.write(str(i)+" "+"3b9f916bbf59021ab781c9f2456df90a0102079f"+"\n")
+
+out_file = open(output_2, "w")
+for i in range(chunks):
+	out_file.write(str(i)+" "+"78585121ee33fbb6666bc4bf1a8498c04b2758e8"+"\n")
+
+out_file = open(output_3, "w")
+for i in range(chunks):
+	out_file.write(str(i)+" "+"3b9f916bbf59021ab781c9f2456df90a0102079f"+"\n")
+for i in range(chunks):
+	out_file.write(str(chunks+i)+" "+"78585121ee33fbb6666bc4bf1a8498c04b2758e8"+"\n")
